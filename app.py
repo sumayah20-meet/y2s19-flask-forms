2f2aa6b216cf7079389daf3578ee6abbce0622fe
myname21-meet <myname21@meet.mit.edu>
2019-07-30 07:06:52 -0400

    finiished

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
 from databases import *
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html', students=query_all())
+   
 
 @app.route('/student/<int:student_id>')
 def display_student(student_id):
     return render_template('student.html', student=query_by_id(student_id))
 
+@app.route('/add' ,methods=['GET', 'POST'])
+def add():
+    if request.method == 'GET':
+        return render_template('add.html')
+    else:
+        name = request.form['student_name']
+        year = request.form['student_year']
+        add_student(name,year)
+        return redirect(url_for('home'))
 #Create an '/add' route here:
 
 if __name__ == '__main__':
